fa42d0ce855a68a6e2db7ed2487226127a87ace0
nikschen <niklaswiemuth@gmail.com>
2020-11-09 17:54:16 +0100

    First Commit

---
+++ src/main/java/de/niklaswiemuth/tryjava/MyFirstStartClass.java
@@ -0,0 +1,10 @@
+package de.niklaswiemuth.tryjava;
+
+
+class MyFirstStartClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/de/niklaswiemuth/tryjava/MyFirstStartClassTest.java
@@ -0,0 +1,12 @@
+package de.niklaswiemuth.tryjava;
+
+class MyFirstStartClassTest {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
