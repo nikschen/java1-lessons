643ded86c8549000bd26d6974d0d011db54bbd30
nikschen <niklaswiemuth@gmail.com>
2020-11-09 18:07:03 +0100

    lesson 2

--- src/main/java/de/niklaswiemuth/tryjava/MyFirstStartClass.java
+++ src/main/java/de/niklaswiemuth/tryjava/MyFirstStartClass.java
@@ -4,7 +4,29 @@ package de.niklaswiemuth.tryjava;
 class MyFirstStartClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        for(int saidNumber=1;saidNumber<=100;saidNumber++)
+        {
+            if (saidNumber%5==0)
+            {
+                if (saidNumber%7==0)
+                {
+                    System.out.println("FitzQuark");
+                }
+                else
+                {
+                    System.out.println("Fitz");
+                }
+            }
+            else if (saidNumber%7==0)
+            {
+                System.out.println("Quark");
+            }
+            else
+            {
+                System.out.println(saidNumber);
+            }
+        }
     }
 
 }
